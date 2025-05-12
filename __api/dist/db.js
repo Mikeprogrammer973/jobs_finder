c7231f26284a7e3bd654570f7f1a33d29d84dfcd
Mike <antiquesclub007@gmail.com>
2025-05-12 08:18:04 -0400

    ...

--- __api/dist/db.js
+++ __api/dist/db.js
@@ -7,7 +7,7 @@ exports.connectDB = void 0;
 const mongoose_1 = __importDefault(require("mongoose"));
 const connectDB = async () => {
     try {
-        await mongoose_1.default.connect(process.env.MONGO_URI || 'mongodb+srv://antiquesclub007:inazuma15@portofolio.rmyk5uq.mongodb.net/?retryWrites=true&w=majority&appName=portofolio');
+        await mongoose_1.default.connect(process.env.MONGO_URI || '');
         console.log('MongoDB conectado');
     }
     catch (err) {
