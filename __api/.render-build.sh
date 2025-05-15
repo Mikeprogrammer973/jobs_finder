26142bcb975e089e17793ded83f359c97f10e232
Mike <antiquesclub007@gmail.com>
2025-05-14 21:24:31 -0400

    :::

--- __api/.render-build.sh
+++ __api/.render-build.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 npm install
 
 # 2. Baixa o Chromium Linux x64 (versão compatível com Puppeteer)
-CHROMIUM_URL="https://commondatastorage.googleapis.com/chromium-browser-snapshots/Linux_x64/1135559/chrome-linux.zip"
+CHROMIUM_URL="https://commondatastorage.googleapis.com/chromium-browser-snapshots/Linux_x64/1460435/chrome-linux.zip"
 wget "$CHROMIUM_URL" -O /tmp/chromium.zip
 unzip /tmp/chromium.zip -d ./local-chromium
 rm /tmp/chromium.zip
